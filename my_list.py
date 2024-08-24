ceca54c9f2fc46c4cbbb5496e6612ed64829a1a8
KazimMack <kazimmack816@gmail.com>
2024-08-24 16:40:09 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,24 @@
+# An empty list 
+my_list = []
+
+# Appended elements 
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Inserted value
+my_list.insert(10, 15)
+
+# Extended list
+my_list.extend([50, 60, 70])
+
+# Last element
+my_list.pop()
+
+# Sorted list
+my_list.sort()
+
+# index value of 30
+index_of_30 = my_list.index(30)
+print(f"index value of 30 in my_list is: {index_of_30}")
